f134e6e1b5152c5fc3e00d6dfd8ad12b956cd9d1
atronov <atronov@gmail.com>
2016-04-14 18:45:24 +0300

    Test

--- test/svg2png-many.test.js
+++ test/svg2png-many.test.js
@@ -1,1 +1,11 @@
-const require
+'use strict';
+
+const svg2png = require('../index');
+
+svg2png.svg2PngDir('../tests', '../results')
+    .then(() => console.log('Done'), errors => {
+        if (!Array.isArray(errors)) {
+            errors = [errors];
+        }
+        errors.forEach(err => console.error(err.stack || err));
+    });
