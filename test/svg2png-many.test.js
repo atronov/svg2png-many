4230cd0050d8be0a0cc892be32ad3aa15b915f6f
atronov <atronov@gmail.com>
2016-04-15 00:49:58 +0300

    fix

--- test/svg2png-many.test.js
+++ test/svg2png-many.test.js
@@ -2,7 +2,7 @@
 
 const svg2png = require('../index');
 
-svg2png.svg2PngDir('../tests', '../results')
+svg2png.svg2PngDir('../tests', '../results', {width:500})
     .then(() => console.log('Done'), errors => {
         if (!Array.isArray(errors)) {
             errors = [errors];
